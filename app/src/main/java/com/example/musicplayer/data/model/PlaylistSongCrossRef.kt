5af98397aa294dd8bc790c845ce94a6e3b8296df
swasray2004 <154862450+swasray2004@users.noreply.github.com>
2025-11-11 18:48:12 +0530

    Delete app directory

--- app/src/main/java/com/example/musicplayer/data/model/PlaylistSongCrossRef.kt
+++
@@ -1,9 +0,0 @@
-package com.example.musicplayer.data.model
-
-import androidx.room.Entity
-
-@Entity(tableName = "playlist_song_cross_ref", primaryKeys = ["playlistId", "songId"])
-data class PlaylistSongCrossRef(
-    val playlistId: Long,
-    val songId: Long
-)
--- app/src/main/java/com/example/musicplayer/data/model/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.musicplayer.data.model
-
-data class User(
-    val uid: String = "",
-    val email: String = "",
-    val displayName: String = "",
-    val photoUrl: String = ""
-)
